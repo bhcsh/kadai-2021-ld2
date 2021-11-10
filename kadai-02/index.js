b40f502da259288e027cf3a042ab7b9387ad07f7
bhcsh <cunshangg53@gmail.com>
2021-11-10 10:36:58 +0000

    課題2を作成

---
+++ kadai-02/index.js
@@ -0,0 +1,12 @@
+const firstName = "hitosi"
+const familyName = "matsumoto"
+console.log(firstName + familyName)
+
+const sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+console.log(sum)
+
+const dog = false
+const cat = true
+console.log(dog || cat)
+console.log(dog && cat)
+console.log(!dog && !cat)
